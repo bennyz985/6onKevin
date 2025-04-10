1784bbc1113fa0ce39f7f04beecad9c5dfe86e39
Ben Zuckerman <benjamin.zuckerman@gmail.com>
2025-04-10 16:59:01 -0500

    data modeling change

--- Build_Graph_Structure/relationships.py
+++ Build_Graph_Structure/relationships.py
@@ -1,3 +1,5 @@
+
+
 from neo4j import GraphDatabase
 import csv
 import time
@@ -8,14 +10,12 @@ username = "neo4j"
 password = "SixKevin"
 driver = GraphDatabase.driver(uri, auth=(username, password))
 
-def create_role_nodes_batch(tx, batch):
+def create_played_role_relationships_batch(tx, batch):
     query = """
     UNWIND $batch AS row
+    MATCH (p:Person {nconst: row['nconst']})
     MATCH (m:Movie {tconst: row['tconst']})
-    CREATE (r:Role {
-        tconst: row['tconst'],
-        nconst: row['nconst'],
-        category: row['category'],
+    CREATE (p)-[:PLAYED_ROLE_IN {
         job: CASE
             WHEN row['category'] = 'director' THEN 'director'
             WHEN row['category'] = 'writer' THEN 'writer'
@@ -28,15 +28,14 @@ def create_role_nodes_batch(tx, batch):
             WHEN row['characters'] IS NOT NULL AND row['characters'] <> '\\N' THEN apoc.convert.fromJsonList(row['characters'])
             ELSE null
         END
-    })
+    }]->(m)
     """
     tx.run(query, batch=batch)
 
-def create_role_indexes(tx):
-    tx.run("CREATE INDEX role_tconst FOR (r:Role) ON (r.tconst)")
-    tx.run("CREATE INDEX role_nconst FOR (r:Role) ON (r.nconst)")
-    tx.run("CREATE INDEX role_tconst_nconst FOR (r:Role) ON (r.nconst, r.tconst)")
-    print("Indexes created for Role.tconst and Role.nconst")
+def create_played_role_relationship_indexes(tx):
+    tx.run("CREATE OR REPLACE INDEX played_role_person_nconst FOR (p:Person)-[r:PLAYED_ROLE_IN]-(m:Movie) ON (p.nconst)")
+    tx.run("CREATE OR REPLACE INDEX played_role_movie_tconst FOR (p:Person)-[r:PLAYED_ROLE_IN]-(m:Movie) ON (m.tconst)")
+    print("Indexes created or replaced for PLAYED_ROLE_IN relationships based on Person.nconst and Movie.tconst")
 
 file_path = "/Users/benzuckerman/Documents/GitHub/7onKevin/Data_Files/princpals.tsv"
 batch_size = 10000
@@ -50,28 +49,25 @@ with open(file_path, 'r', encoding='utf-8') as tsvfile:
     with driver.session() as session:
         for row in reader:
             category = row['category']
-            characters_str = row['characters'].strip()  #remove whitespace
+            characters_str = row['characters'].strip() 
 
             if category in ['actor', 'actress', 'director', 'writer'] or (category == 'self' and characters_str != '\\N' and characters_str != '"Self"'):
                 batch.append(row)
                 if len(batch) >= batch_size:
-                    session.execute_write(create_role_nodes_batch, batch)
+                    session.execute_write(create_played_role_relationships_batch, batch)
                     total_processed += len(batch)
                     batch = []
 
                     if total_processed % report_interval == 0:
                         elapsed_time = time.time() - start_time
-                        print(f"Processed and attempted to create {total_processed} Role nodes in {elapsed_time:.2f} seconds (only if Title nodes exists).")
+                        print(f"Processed {total_processed} principals and attempted to create PLAYED_ROLE_IN relationships in {elapsed_time:.2f} seconds (only if Person and Movie nodes exist).")
 
         if batch:
-            session.execute_write(create_role_nodes_batch, batch)
+            session.execute_write(create_played_role_relationships_batch, batch)
             total_processed += len(batch)
             elapsed_time = time.time() - start_time
-            print(f"Processed and attempted to create a final {len(batch)} Role nodes in {elapsed_total_time:.2f} seconds (only if Title nodes exists).")
+            print(f"Processed a final {len(batch)} principals and attempted to create PLAYED_ROLE_IN relationships in {elapsed_time:.2f} seconds (only if Person and Movie nodes exist).")
 
-        session.execute_write(create_role_indexes)
-
-elapsed_total_time = time.time() - start_time
-print(f"Total of {total_processed} Role nodes processed (attempted creation only if Title nodes exists) in {elapsed_total_time:.2f} seconds.")
-driver.close()
-print("Driver closed.")
+        session.execute_write(create_played_role_relationship_indexes)
+    driver.close
+    print("Driver Closed.")
